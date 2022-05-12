f1daf5df6d61f4d4e57ab522a26ff06a4fcf5d96
Raphael Jacques <rajacque@c1r2p6.42quebec.com>
2022-05-12 17:45:10 -0400

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: rajacque <rajacque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 14:30:51 by rajacque          #+#    #+#             */
-/*   Updated: 2022/05/12 14:04:37 by rajacque         ###   ########.fr       */
+/*   Updated: 2022/05/12 17:37:25 by rajacque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (!lst || !f)
 		return (NULL);
-	if (!(new_elem == ft_lstnew(f(lst->content))))
-	{
-		ft_lstclear(&lst, del);
+	new_lst = ft_lstnew(f(lst->content));
+	if (!new_lst)
 		return (NULL);
-	}
-	new_lst = new_elem;
+	new_elem = new_lst;
 	lst = lst->next;
 	while (lst)
 	{
-		if (!(new_elem == ft_lstnew(f(lst->content))))
+		new_lst->next = ft_lstnew(f(lst->content));
+		if (!new_lst->next)
 		{
-			ft_lstclear(&lst, del);
-			ft_lstclear(&new_lst, del);
-			break ;
+			ft_lstclear(&new_elem, del);
+			return (0);
 		}
+		new_lst = new_lst->next;
 		lst = lst->next;
-		ft_lstadd_back(&new_lst, new_elem);
 	}
-	return (new_lst);
+	new_lst->next = NULL;
+	return (new_elem);
 }
